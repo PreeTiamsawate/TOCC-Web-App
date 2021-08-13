427385b52e0f79a1ff17291121852d66ac6f0083
Pree Tiamsawate <preetiamsawate@gmail.com>
2021-08-13 16:19:08 +0700

    Pilot page font size imporved

--- sidebar_control.js
+++ sidebar_control.js
@@ -7,3 +7,13 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   //   document.getElementById("main").style.marginLeft = "0";
 }
+
+const currentLocation = location.href;
+const navLinks = document.querySelectorAll(".nav-link");
+for (navLink of navLinks) {
+  if (navLink.href === currentLocation) {
+    navLink.classList.add("nav-active");
+
+    navLink.setAttribute("aria-current", "page");
+  }
+}
